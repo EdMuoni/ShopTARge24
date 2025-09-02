50bca4a632c8ebef2b96af0ac6382272c0efe2c9
Opilane TTHK <opilane@tthk.ee>
2025-09-02 11:15:22 +0300

    Update
    Created readonly ShopTarge24ShopContext

--- ShopTARge24/Controllers/SpaceshipsController.cs
+++ ShopTARge24/Controllers/SpaceshipsController.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge24.Data;
+
 
 namespace ShopTARge24.Controllers
 {
+
+
+
     public class SpaceshipsController : Controller
     {
+        private readonly ShopTARge24Context _Context;
+
+        public SpaceshipsController
+            (
+                ShopTARge24Context context
+            )
+        {
+            _Context = context;
+        }
         public IActionResult Index()
         {
+            var result = 
+
             return View();
         }
     }
---
+++ ShopTARge24/Models/Spaceships/SpaceshipIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopTARge24.Models.Spaceships
+{
+    public class SpaceshipIndexViewModel
+    {
+        public Guid? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Classification { get; set; }
+        public string? Size { get; set; }
+        public DateTime? BuiltDate { get; set; } = default(DateTime?);
+        public int? Crew { get; set; }
+        public int? EnginePower { get; set; }
+    }
+}
