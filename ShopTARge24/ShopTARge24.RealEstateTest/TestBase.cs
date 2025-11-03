28c789b8c827367a6c648dbb37c94d64fea7345c
Edgar <edgar.muoni@gmail.com>
2025-11-03 09:20:45 +0200

    Created all necessary files.

---
+++ ShopTARge24/ShopTARge24.RealEstateTest/TestBase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ShopTARge24.RealEstateTest
+{
+    public abstract class TestBase
+    {
+        protected IServiceProvider serviceProvider { get; set; }
+        protected TestBase()
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+            serviceProvider = services.BuildServiceProvider();
+
+        }
+
+        public virtual void SetupServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        private void RegisterMacros(IServiceCollection services)
+        {
+            var macrobaseType = typeof(IMacros);
+        }
+    }
+}
