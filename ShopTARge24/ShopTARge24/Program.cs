0caf7eba36b81e50e805c2c3eb9457fe793ac0d8
Edgar <edgar.muoni@gmail.com>
2025-11-11 09:50:28 +0200

    Update

--- ShopTARge24/ShopTARge24/Program.cs
+++ ShopTARge24/ShopTARge24/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<ISpaceshipServices, SpaceshipServices>();
 builder.Services.AddScoped<IFileServices, FileServices>();
 builder.Services.AddScoped<IRealEstateServices, RealEstateServices>();
-builder.Services.AddScoped<IWeatherForecastServices, WeatherForecastServices>();
+//builder.Services.AddScoped<IWeatherForecastServices, WeatherForecastServices>();
 
 
 builder.Services.AddDbContext<ShopTARge24Context>(options =>
