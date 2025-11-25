61afdd6c7de7948138160db19acf310bf37a57c3
Edgar <edgar.muoni@gmail.com>
2025-11-25 11:26:31 +0200

    Email

---
+++ ShopTARge24/ShopTARge24.Core/Dto/EmailDto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopTARge24.Core.Dto
+{
+    public class EmailDto
+    {
+        public string? To { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+        public List<IFormFile> Attachment { get; set; } = new List<IFormFile>();
+
+    }
+}
